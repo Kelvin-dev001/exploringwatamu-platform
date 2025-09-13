d0c5df110ac1c3f4a57931d27f95be322409d76f
Kelvin-dev001 <kelvinoyugi101@gmail.com>
2025-09-13 13:34:04 +0300

    serviceroutes

--- backend/src/routes/serviceBookingRoutes.js
+++ backend/src/routes/serviceBookingRoutes.js
@@ -5,6 +5,5 @@ const authAdmin = require('../middleware/authAdmin');
 router.post('/book', serviceBookingController.bookService); // User booking
 router.get('/', authAdmin, serviceBookingController.getServiceBookings); // Admin list bookings
 router.put('/:id/cancel', authAdmin, serviceBookingController.cancelBooking); // Admin cancel
-router.post('/check', serviceBookingController.checkAvailability); // POST /servicebookings/check
-
+router.post('/check', serviceBookingController.checkAvailability); // POST /servicebookings/chec
 module.exports = router;
